80bdb542cf7bdfb207387f9e4ecbddf0e9a5fba0
Allison _ <allimacq@gmail.com>
2021-01-27 18:17:56 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,9 @@
 # Create your costume_stores migration here
+class CreateCostumeStore < ActiveRecord::Migration[5.2]
+  
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.int

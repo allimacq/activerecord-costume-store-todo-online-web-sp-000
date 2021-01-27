872122cf485e143dc5bf74d7d09a171f3fd0289a
Allison _ <allimacq@gmail.com>
2021-01-27 18:20:53 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,2 +1,10 @@
 # Create your haunted_houses migration here
-class CreateHauntedHouses
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t
